1b74ea15ff2ecbfb0adbec5b4480747dc4573af6
adamdziedzic <adamtomaszdziedzic@gmail.com>
2022-05-15 18:33:59 +0200

    parallel stream

--- com/company/ParallelStreamExample.java
+++ com/company/ParallelStreamExample.java
@@ -9,12 +9,24 @@ public class ParallelStreamExample {
     public static void main(String[] args) {
         Random random = new Random();
         List<Double> list = random.doubles(1_000).boxed().collect(Collectors.toList());
+        var doer = new HeavyDoer();
         var time1 = System.currentTimeMillis();
-        list.stream().map(x -> x*2).filter(x -> x > 1).forEach(x1 -> System.out.println("sequential: " + x1 + " " + Thread.currentThread().getName()));
+        list.stream().map(x -> x*2).filter(x -> x > 1).map(doer::doHeavyWork).forEach(x1 -> System.out.println("sequential: " + x1 + " " + Thread.currentThread().getName()));
         var time2 = System.currentTimeMillis();
-        list.parallelStream().map(x -> x*2).filter(x -> x > 1).forEach(x1 -> System.out.println("parallel: " + x1 + " " + Thread.currentThread().getName()));
+        list.parallelStream().map(x -> x*2).filter(x -> x > 1).map(doer::doHeavyWork).forEach(x1 -> System.out.println("parallel: " + x1 + " " + Thread.currentThread().getName()));
         var time3 = System.currentTimeMillis();
 
         System.out.println("normal: " + (time2 - time1) + "ms, parallel:  " + (time3 - time2) + "ms");
+    }
+
+    public static class HeavyDoer {
+        Double doHeavyWork(Double d) {
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return d;
+        }
     }
 }
