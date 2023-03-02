8d7c724dc3c57b56c0d8cc74cdbcbe1453ebef09
Jakub Słowikowski <sekenuw@gmail.com>
2023-03-02 09:12:30 +0100

    ParallelStream last changes

--- com/company/ParallelStreamExample.java
+++ com/company/ParallelStreamExample.java
@@ -10,12 +10,22 @@ public class ParallelStreamExample {
         Random random = new Random();
         List<Double> list = random.doubles(1_000).boxed().collect(Collectors.toList());
         var doer = new HeavyDoer();
-        var time1 = System.currentTimeMillis();
-        list.stream().map(x -> x*2).filter(x -> x > 1).map(doer::doHeavyWork).forEach(x1 -> System.out.println("sequential: " + x1 + " " + Thread.currentThread().getName()));
-        var time2 = System.currentTimeMillis();
-        list.parallelStream().map(x -> x*2).filter(x -> x > 1).map(doer::doHeavyWork).forEach(x1 -> System.out.println("parallel: " + x1 + " " + Thread.currentThread().getName()));
-        var time3 = System.currentTimeMillis();
 
+        var time1 = System.currentTimeMillis();
+        list.stream()
+                .map(x -> x * 2)
+                .filter(x -> x > 1)
+                .map(doer::doHeavyWork)
+                .forEach(x1 -> System.out.println("sequential: " + x1 + " " + Thread.currentThread().getName()));
+
+        var time2 = System.currentTimeMillis();
+        list.parallelStream()
+                .map(x -> x * 2)
+                .filter(x -> x > 1)
+                .map(doer::doHeavyWork)
+                .forEach(x1 -> System.out.println("parallel: " + x1 + " " + Thread.currentThread().getName()));
+
+        var time3 = System.currentTimeMillis();
         System.out.println("normal: " + (time2 - time1) + "ms, parallel:  " + (time3 - time2) + "ms");
     }
 
